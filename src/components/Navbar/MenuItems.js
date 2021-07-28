3be44a7c2f5fcd08276032f66eb19d289912ee27
Ivory Smith <smith.ivory94@gmail.com>
2021-07-28 13:24:07 -0500

    added moving files

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: "#",
+        cName: 'nav-links',
+
+    },
+    {
+        title: 'Find an Observatory',
+        url: "#",
+        cName: 'nav-links',
+
+    },
+    {
+        title: 'Log In',
+        url: "#",
+        cName: 'nav-links',
+
+    },
+    {
+        title: 'Contact Us',
+        url: "#",
+        cName: 'nav-links',
+
+    },
+    {
+        title: 'Sign Up',
+        url: "#",
+        cName: 'nav-links-mobile',
+
+    },
+]
