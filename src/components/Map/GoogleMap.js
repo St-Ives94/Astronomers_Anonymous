1b1881e82f2504b289c0242919980000a506bcca
Ivory Smith <smith.ivory94@gmail.com>
2021-08-08 22:22:48 -0500

    fixing what I broke

--- src/components/Map/GoogleMap.js
+++ src/components/Map/GoogleMap.js
@@ -1,14 +1,34 @@
-// import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps"
+// // import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps"
 
-function Map () {
-    return (
-        <GoogleMap
-            defaultZoom={10}
-            defaultCenter={{ lat: 34.118271, lng: -118.300400}}
-        />
-    );
-}
+// function Map () {
+//     return (
+//         <GoogleMap
+//             defaultZoom={10}
+//             defaultCenter={{ lat: 34.118271, lng: -118.300400}}
+//         />
+//     );
+// }
 
-const WrappedMap = withScriptjs(withGoogleMap(Map));
+// const WrappedMap = withScriptjs(withGoogleMap(Map));
 
-export default WrappedMap
+// export default WrappedMap
+
+// import WrappedMap from '../../Map/GoogleMap'
+
+
+// function Map () {
+//     return (
+//         <GoogleMap
+//             defaultZoom={10}
+//             defaultCenter={{ lat: 34.118271, lng: -118.300400}}
+//         />
+//     );
+// }
+{/* <WrappedMap googleMapUrl={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key={process.env.REACT_APP_GOOGLE_KEY}`} loadingElement={<div style={{height: '100%' }} />}
+containerElement={<div style={{height: '100%' }} />}
+mapElement={<div style={{height: '100%' }} />}
+/> */}
+// defaultZoom={10}
+// defaultCenter={{ lat: 34.118271, lng: -118.300400}}
+// const WrappedMap = withScriptjs(withGoogleMap(Map));
+
--- src/components/pages/homepage/Home.js
+++ src/components/pages/homepage/Home.js
@@ -1,58 +1,13 @@
 import React from 'react';
 import Hero from '../../Hero';
-import { GoogleMap, useLoadScript, InfoWindow } from "react-google-map";
 import { homeObjOne, homeObjThree,
     homeObjTwo, homeObjFour} from './Data';
-// import WrappedMap from '../../Map/GoogleMap'
-
-
-// function Map () {
-//     return (
-//         <GoogleMap
-//             defaultZoom={10}
-//             defaultCenter={{ lat: 34.118271, lng: -118.300400}}
-//         />
-//     );
-// }
-{/* <WrappedMap googleMapUrl={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key={process.env.REACT_APP_GOOGLE_KEY}`} loadingElement={<div style={{height: '100%' }} />}
-containerElement={<div style={{height: '100%' }} />}
-mapElement={<div style={{height: '100%' }} />}
-/> */}
-// defaultZoom={10}
-// defaultCenter={{ lat: 34.118271, lng: -118.300400}}
-const WrappedMap = withScriptjs(withGoogleMap(Map));
-
 function Home() {
-    const libraries = ['places'];
-    const mapContainerStyle = {
-        width: '100vw',
-        height: '100vh'
-    };
-    const center = {
-        lat: 34.118271,
-        lng: -118.300400
-    }
-    const {isLoaded, loadError} = useLoadScript({
-        googleMapsApi: process.env.REACT_APP_GOOGLE_KEY,
-        libraries,
-    });
-
-    if(loadError) return 'Error loading maps';
-    if(!isLoaded) return 'Loading Maps'
     return (
         <>
             <Hero {...homeObjOne} />
             <Hero {...homeObjThree} />
         <Hero {...homeObjTwo}/>
-        <div>
-        <GoogleMap
-            mapContainerStyle={mapContainerStyle}
-            zoom={10}
-            center={center}
-            >
-
-        </GoogleMap>
-        </div>
         {/* <Pricing /> */}
         <Hero {...homeObjFour} />
         </>
